a0be33fd44c526bc7059f8584fd0f4de58ef53a0
linuxmaster0312 <59486852+linuxmaster0312@users.noreply.github.com>
2020-08-24 17:30:07 -0400

    Create obtblock.sh

---
+++ obtblock.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+#assuming wget exist on image
+#obtain dnsmasq block list and set configuration
+
+wget https://github.com/notracking/hosts-blocklists/raw/master/dnsmasq/dnsmasq.blacklist.txt
+
+
+
