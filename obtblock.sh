84ab8b61024973b396580d0d6c11563b49c429f0
linuxmaster0312 <59486852+linuxmaster0312@users.noreply.github.com>
2020-08-25 10:02:29 -0400

    Update obtblock.sh

--- obtblock.sh
+++ obtblock.sh
@@ -1,8 +1,16 @@
-#/bin/bash
+#/usr/bin/env bash
+
 #assuming wget exist on image
 #obtain dnsmasq block list and set configuration
 
 wget https://github.com/notracking/hosts-blocklists/raw/master/dnsmasq/dnsmasq.blacklist.txt
+
+DIR=$(pwd)
+echo "conf-file=${DIR}/dnsmasq.blacklist.txt
+
+systemctl restart dnsmasq
+
+exit 0
 
 
 
