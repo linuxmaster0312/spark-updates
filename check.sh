11d526dcec73a527991a72cf1607e20c6608abfd
linuxmaster0312 <59486852+linuxmaster0312@users.noreply.github.com>
2020-09-07 14:56:20 -0400

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 #checks for updates from the image side
 
-cd /spark/updates
+cd /spark
 git clone https://github.com/linuxmaster0312/Spark-Blocking.git
-
-if `grep -q "1" 
+cd ./Spark-Blocking
+if `grep -q "1" trigger.txt`; then
+  chmod +x update.sh
+  ./update.sh
+else
+  echo 'There are no updates as of yet'
+fi
